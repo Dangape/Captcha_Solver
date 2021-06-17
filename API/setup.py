89611c318e320d32613530f7296dc45bd47d8e2f
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-06-17 15:32:25 -0300

    save

--- API/setup.py
+++ API/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='CAPTCHA ML',
+    name='CAPTCHA_ML',
     description="Translate CAPTCHA images into text",
     author="Daniel Bemerguy",
     author_email="daniel06197@gmail.com",
-    install_requires=['flask', 'numpy','string','pytesseract','opencv-python','imageio','io','base64','pillow','imutils'],
+    install_requires=['flask','numpy','pytesseract','opencv-python','imageio','pillow','imutils','uwsgi'],
     python_requires='>=3.6',
 )
